ec71507ae892b0b781f72e39a54951269b775e1b
John Fallows <john.fallows@kaazing.com>
2018-12-26 18:21:26 -0600

    Refactor Nukleus and introduce Elektron

---
+++ src/main/java/org/reaktivity/nukleus/sse/internal/SseElektron.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2016-2018 The Reaktivity Project
+ *
+ * The Reaktivity Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.reaktivity.nukleus.sse.internal;
+
+import static java.util.Collections.singletonMap;
+import static org.reaktivity.nukleus.route.RouteKind.SERVER;
+
+import java.util.Map;
+
+import org.reaktivity.nukleus.Elektron;
+import org.reaktivity.nukleus.route.RouteKind;
+import org.reaktivity.nukleus.sse.internal.stream.ServerStreamFactoryBuilder;
+import org.reaktivity.nukleus.stream.StreamFactoryBuilder;
+
+final class SseElektron implements Elektron
+{
+    private final Map<RouteKind, StreamFactoryBuilder> streamFactoryBuilders;
+
+    SseElektron(
+        SseConfiguration config)
+    {
+        this.streamFactoryBuilders = singletonMap(SERVER, new ServerStreamFactoryBuilder(config));
+    }
+
+    @Override
+    public StreamFactoryBuilder streamFactoryBuilder(
+        RouteKind kind)
+    {
+        return streamFactoryBuilders.get(kind);
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("%s %s", getClass().getSimpleName(), streamFactoryBuilders);
+    }
+}
